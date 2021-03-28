53d081f90a49ee0e48902e8b8d5ff5d41c08545f
Your Name <you@example.com>
2021-03-28 19:44:21 +0700

    U

---
+++ product.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .container {
+            width: 960px;
+            margin: 0 auto;
+            height: auto;
+            text-align: center;
+        }
+
+        table {
+            text-align: center;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="conatainer">
+        <h2>จัดการสินค้า</h2>
+    </div>
+</body>
+</html>
