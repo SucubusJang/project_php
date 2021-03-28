13327c0ff7c669ef60aa2532965f0d16d169f8ea
Your Name <you@example.com>
2021-03-28 20:38:13 +0700

    M

--- product.php
+++ product.php
@@ -21,9 +21,147 @@
     </style>
 </head>
 
-<body>
-    <div class="conatainer">
+<body onload="loadContent()">
+    <div class="container">
         <h2>จัดการสินค้า</h2>
+        <button onclick="show_add()">เพิ่มสินค้า</button>
+        <Table>
+            <thead>
+                <th>รหัสสินค้า</th>
+                <th>ชื่อสินค้า</th>
+                <th>ราคาสินค้า</th>
+                <th>จำนวน</th>
+                <th>จัดการ</th>
+            </thead>
+            <tbody id="show_product">
+
+            </tbody>
+        </Table>
+        <div id="out" style="margin-top: 50px;"></div>
     </div>
+    <script>
+        function loadContent() {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                console.log(this.readyState + ", ", this.status);
+                if (this.readyState == 4 && this.status == 200) {
+                    // console.log(this.responseText);
+                    data = JSON.parse(this.responseText);
+                    create_Table(data);
+                }
+            }
+            xhttp.open("GET", "product_rest.php?show_pro=show_pro", true);
+            xhttp.send();
+        }
+
+        function create_Table(data) {
+            out = document.getElementById("show_product");
+            // console.log(data.length);
+            text = "";
+            for (i = 0; i < data.length; i++) {
+                text += "<tr>";
+                for (info in data[i]) {
+                    text += "<td>" + data[i][info] + "</td>";
+                }
+                text += "<td><button onclick='edit_pro(" + data[i].id + ")'>แก้ไข</button> <button onclick='del_pro(" + data[i].id + ")'>ลบสินค้า</button></td>";
+                text += "</tr>\n";
+            }
+            out.innerHTML = text;
+        }
+
+        function show_add() {
+            out = document.getElementById("out");
+            text = "";
+            text = "<table border='1'>";
+            text += "<tr><td><label>ชื่อสินค้า</label></td>";
+            text += "<td><input type='text' name='' id='name'></td></tr>";
+            text += "<tr><td><label>ราคาสินค้า</label></td>";
+            text += "<td><input type='number' name='' id='price'></td></tr>";
+            text += "<tr><td><label>จำนวนสินค้า</label></td>";
+            text += "<td><input type='number' name='' id='stock'></td></tr>";
+            text += "<tr><td colspan='2'><button onclick='add_pro()'>เพิ่มสินค้า</button></td></tr>";
+            text += "</table>";
+            out.innerHTML = text;
+        }
+
+        function add_pro() {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                // console.log(this.readyState + ", ", this.status);
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
+                }
+            }
+            xhttp.open("POST", "product_rest.php", true);
+            xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            name_pro = document.getElementById("name");
+            price_pro = document.getElementById("price");
+            stock_pro = document.getElementById("stock");
+            xhttp.send("name_pro=" + name_pro.value + "&price_pro=" + price_pro.value + "&stock_pro=" + stock_pro.value);
+            name_pro.value = null;
+            price_pro.value = null;
+            stock_pro.value = null;
+            window.location.href = "product.php";
+        }
+        function edit_pro(idx) {
+           // alert(idx);
+            label = ['ชื่อสินค้า', 'ราคา', 'จำนวนสินค้า'];
+            Ids = ['name', 'price', 'stock'];
+            type = ['text','number','number'];
+            out = document.getElementById("out");
+            let xhttp = new XMLHttpRequest();
+            text = "";
+            j = 0;
+            xhttp.onreadystatechange = function() {
+                // console.log(this.readyState + ", ", this.status);
+                if (this.readyState == 4 && this.status == 200) {
+                     console.log(this.responseText);
+                    data = JSON.parse(this.responseText);
+                    text = "<table border='1'>";
+                    for (i = 0; i < data.length; i++) {
+                        text += "<tr>";
+                        for (info in data[i]) {
+                            text += "<tr><td>" + label[j] + "</td><td><input type='"+type[j]+"' name='' id='" + Ids[j] + "' value='" + data[i][info] + "'></td></tr>";
+                            j++;
+                        }
+                        text += "</tr>";
+                    }
+                    text += "<tr><td colspan='2'><button onclick='edit_data(" + idx + ")'>Edit</button></td></tr>";
+                    out.innerHTML = text + "</table>";
+                }
+            }
+            xhttp.open("GET", "product_rest.php?edit_Id=" + idx + "", true);
+            xhttp.send();
+        }
+
+        function edit_data(idx) {
+            //  alert(idx);
+            name = document.getElementById("name").value;
+            price = document.getElementById("price").value;
+            stock = document.getElementById("stock").value;
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                // console.log(this.readyState + ", ", this.status);
+                console.log(this.responseText);
+            }
+            xhttp.open("POST", "product_rest.php", true);
+            xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhttp.send("name=" + name + "&price=" + price + "&stock=" + stock + "&Id=" + idx);
+            window.location.href = "product.php";
+        }
+
+        function del_pro(idx) {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                     console.log(this.responseText);
+                }
+            }
+            xhttp.open("GET", "product_rest.php?del_Id=" + idx + "", true);
+            xhttp.send();
+            window.location.href = "product.php";
+        }
+    </script>
 </body>
+
 </html>
--- product_rest.php
+++ product_rest.php
@@ -1,10 +1,57 @@
 <?php
-    include_once("dbcontrol.php");
+    include_once("dbcontroller.php");
     include_once("util.php");
     $debug_mode = false;
     if($_SERVER['REQUEST_METHOD'] == "GET"){
-        debug_text("For Get Method" ,$debug_mode); 
+        // debug_text("For Get Method" ,$debug_mode); 
+        if(isset($_GET['show_pro'])){
+           echo json_encode(show_product($debug_mode));
+        }else if(isset($_GET['del_Id'])){
+            $Id = $_GET['del_Id'];
+            del_product($Id,$debug_mode);
+        }else if(isset($_GET['edit_Id'])){
+            $Id = $_GET['edit_Id'];
+            echo json_encode(show_productById($Id,$debug_mode));
+        }
     }else if($_SERVER['REQUEST_METHOD'] == "POST"){
         debug_text("For POST Method" ,$debug_mode); 
+        if(isset($_POST['name_pro'])){
+            $name_pro = $_POST['name_pro'];
+            $price_pro = $_POST['price_pro'];
+            $stock_pro = $_POST['stock_pro'];
+            insert_product($name_pro,$price_pro,$stock_pro,$debug_mode);
+        }else if(isset($_POST['name'])){
+            $Id = $_POST['Id'];
+            $name = $_POST['name'];
+            $price = $_POST['price'];
+            $stock = $_POST['stock'];  
+            update_product($Id,$name,$price,$stock,$debug_mode);
+        }
+    }
+    function show_product($debug_mode){
+        $mydb = new db("root","","shopping" ,$debug_mode);
+        $data = $mydb->query("SELECT * FROM `product`");
+        return $data;
     }
 
+    function insert_product($name_pro,$price_pro,$stock_pro,$debug_mode) {
+        $mydb = new db("root","","shopping" ,$debug_mode);
+        $data = $mydb->query_only("INSERT INTO `product`(`id`, `name`, `price`, `stock`) VALUES (null,'{$name_pro}','{$price_pro}','{$stock_pro}')");
+        return $data;
+    }
+    function del_product($Id,$debug_mode){
+        $mydb = new db("root","","shopping", $debug_mode);
+        $data = $mydb->query_only("DELETE FROM `product` WHERE `id` = '{$Id}'");
+        return $data;
+    }
+    function show_productById($Id,$debug_mode){
+        $mydb = new db("root","","shopping" ,$debug_mode);
+        $data = $mydb->query("SELECT `name`, `price`, `stock` FROM `product` WHERE `id` = '{$Id}'");
+        return $data;
+    }
+    function update_product($Id,$name,$price,$stock,$debug_mode){
+        $mydb = new db("root","","shopping", $debug_mode);
+        $data = $mydb->query_only("UPDATE `product` SET `name`='{$name}',`price`='{$price}',`stock`='{$stock}' WHERE `id`='{$Id}'");
+        return $data;
+    }
+?>
