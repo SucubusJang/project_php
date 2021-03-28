0bfebbbb59ab7696d995f75fc16db3aec0318f0b
Your Name <you@example.com>
2021-03-28 19:40:10 +0700

    M

--- product_rest.php
+++ product_rest.php
@@ -1,8 +1,10 @@
 <?php
     include_once("dbcontrol.php");
+    include_once("util.php");
+    $debug_mode = false;
     if($_SERVER['REQUEST_METHOD'] == "GET"){
-        
+        debug_text("For Get Method" ,$debug_mode); 
     }else if($_SERVER['REQUEST_METHOD'] == "POST"){
-       
+        debug_text("For POST Method" ,$debug_mode); 
     }
 
