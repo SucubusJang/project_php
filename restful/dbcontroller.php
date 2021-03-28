f66a4c54c5ac5c9831ce1e7ceae43d27656befa8
Your Name <you@example.com>
2021-03-28 19:07:25 +0700

    U

---
+++ restful/dbcontroller.php
@@ -0,0 +1,38 @@
+<?php
+    class db{
+        private $db;
+        private $debug;
+        function __construct($user, $pass, $dbname, $debug){
+            $this->debug = $debug;
+            $this->db = new mysqli("localhost",$user,$pass,$dbname);
+            $this->db->set_charset("utf8");
+            // Check Connection
+            if($this->db->connect_errno){
+                echo "Fail to connect to MYSQL: ".$this->db->connect_error;
+                exit();
+            }else{
+                $this->debug_text("Connect Sucess......!");
+            }
+        }
+       function debug_text($text){
+            if($this->debug){
+                echo "Debug :{$text}\n";
+            }
+       }
+
+       function query($sql){
+           $result = $this->db->query($sql);
+           $this->debug_text($sql);
+           $data = $result->fetch_all(MYSQLI_ASSOC);
+           return $data;
+       }
+
+       function query_only($sql){
+           $result = $this->db->query($sql);
+       }
+
+       function close(){
+           $this->db->close();
+       }
+    }
+?>
