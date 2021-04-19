08a0ab42e84da035071f907dc6ab995f07d694db
Your Name <you@example.com>
2021-04-19 23:06:00 +0700

    U

---
+++ shopping.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 19, 2021 at 11:05 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
+
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shopping`
+--
+CREATE DATABASE IF NOT EXISTS `shopping` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `shopping`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `date_purchase` timestamp NOT NULL DEFAULT current_timestamp(),
+  `total` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `orders`
+--
+
+TRUNCATE TABLE `orders`;
+--
+-- Dumping data for table `orders`
+--
+
+INSERT DELAYED IGNORE INTO `orders` (`id`, `date_purchase`, `total`, `status`) VALUES
+(1, '2021-03-29 16:01:38', 1, 1),
+(2, '2021-03-29 16:03:41', 1, 1),
+(3, '2021-03-30 08:34:35', 1, 1),
+(4, '2021-04-16 14:13:07', 3, 1),
+(5, '2021-04-16 15:24:27', 10, 1),
+(6, '2021-04-16 15:27:32', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_detail`
+--
+
+DROP TABLE IF EXISTS `order_detail`;
+CREATE TABLE `order_detail` (
+  `id_product` int(11) NOT NULL,
+  `id_orders` int(11) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `order_detail`
+--
+
+TRUNCATE TABLE `order_detail`;
+--
+-- Dumping data for table `order_detail`
+--
+
+INSERT DELAYED IGNORE INTO `order_detail` (`id_product`, `id_orders`, `amount`) VALUES
+(1, 1, 5),
+(1, 2, 6),
+(1, 3, 1),
+(1, 4, 2),
+(1, 5, 3),
+(1, 6, 1),
+(2, 2, 4),
+(2, 4, 1),
+(2, 5, 3),
+(2, 6, 1),
+(3, 2, 2),
+(3, 5, 2),
+(5, 5, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `product`
+--
+
+TRUNCATE TABLE `product`;
+--
+-- Dumping data for table `product`
+--
+
+INSERT DELAYED IGNORE INTO `product` (`id`, `name`, `price`, `stock`) VALUES
+(1, 'ปากกา', 10, 10),
+(2, 'ยางลบ', 10, 10),
+(3, 'เกม', 100, 10),
+(5, 'ขวดน้ำ', 7, 10),
+(6, 'ไม้บรรทัด', 25, 10);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD PRIMARY KEY (`id_product`,`id_orders`),
+  ADD KEY `id_orders` (`id_orders`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD CONSTRAINT `order_detail_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `order_detail_ibfk_2` FOREIGN KEY (`id_orders`) REFERENCES `orders` (`id`);
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
